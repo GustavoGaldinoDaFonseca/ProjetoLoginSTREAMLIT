f2f918213249906e1bf097f15a5e5af922396a4d
gustavo <00001111060563sp@al.educacao.sp.gov.br>
2025-08-12 08:55:03 -0300

    codigos

--- utils/validar_email.py
+++ utils/validar_email.py
@@ -2,4 +2,4 @@ import re
 
 def validar_email(email):
     email_padrao = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-    return re.match(email_padrao) is not None
+    return re.match(email_padrao, email) is not None
